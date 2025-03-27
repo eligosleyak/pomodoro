b99a6677634c91174c3425d3c25fcdbf67df3dbb
Shashank Acharya <shashankchr@protonmail.ch>
2025-03-27 09:30:40 +0545

    Edited Logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Pomodoro Timer',  // Changed title
+  description: 'A simple Pomodoro Timer to help you stay productive.',
   generator: 'v0.dev',
+  icons: {
+    icon: '/pomodoro-icon.svg',  // Path to the SVG icon
+  },
 }
 
 export default function RootLayout({
@@ -14,6 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/pomodoro-icon.svg" type="image/svg+xml" />  {/* Added favicon */}
+      </head>
       <body>{children}</body>
     </html>
   )
